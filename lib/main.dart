abab3b0b2620d7a8bfa79f42d63ea3f6a644abac
mohamedbadr <78334225+mohamedbadrco@users.noreply.github.com>
2022-07-23 13:26:55 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1039,25 +1039,27 @@ Enter the number of columns or the number of charctars per
           goback
         ]));
     var mesgimg = Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-          Container(
-            height: 50,
-            margin: const EdgeInsets.all(15.0),
-            padding: const EdgeInsets.all(1.0),
-            decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(25)),
-                color: Colors.black.withOpacity(0.1)),
-            child: Center(
-                child: Text('your image was  saved as $name.png',
-                    style: const TextStyle(
-                        color: Colors.white70, fontWeight: FontWeight.bold))),
-          ),
-          links1,
-          goback,
-        ]));
+        child: Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 50,
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(1.0),
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  color: Colors.black.withOpacity(0.1)),
+              child: Center(
+                  child: Text('your image was  saved as $name.png',
+                      style: const TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold))),
+            ),
+            links1,
+            goback,
+          ]),
+    ));
 
     var home = Column(children: [
       Container(
@@ -1202,138 +1204,137 @@ Enter the number of columns or the number of charctars per
                     ],
                   )),
               Container(
-                margin: const EdgeInsets.all(10.0),
-                child: ClipRect(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ClipRect(
                     child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(25)),
-                        color: const Color(0xff22273E).withOpacity(0.7)),
-                    child: Expanded(
-                      child: SizedBox(
+                      filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                      child: Container(
                         width: MediaQuery.of(context).size.width,
-                        child: imagebytes == null
-                            ? SizedBox(
-                                height: MediaQuery.of(context).size.height - 90,
-                                child: done == false ? home : mesgtxt)
-                            : Column(
-                                children: [
-                                  Container(
-                                    width: MediaQuery.of(context).size.width,
-                                  ),
-                                  Container(
-                                      child: prograss == false
-                                          ? Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                Image.memory(
-                                                  imagebytes!,
-                                                  width: MediaQuery.of(context)
-                                                      .size
-                                                      .width,
-                                                  alignment: Alignment.center,
-                                                ),
-                                                Container(
-                                                  child: done == false
-                                                      ? Column(
-                                                          children: [
-                                                            change,
-                                                            Column(
-                                                              children: [
-                                                                // ignore: prefer_const_constructors
-                                                                Text(
-                                                                  "output type",
-                                                                  style: const TextStyle(
-                                                                      color: Colors
-                                                                          .white60,
-                                                                      fontSize:
-                                                                          16,
-                                                                      fontWeight:
-                                                                          FontWeight
-                                                                              .bold),
-                                                                ),
-                                                                Wrap(
-                                                                  spacing: 3,
-                                                                  runSpacing: 3,
-                                                                  children:
-                                                                      typeList,
-                                                                ),
-                                                              ],
-                                                            ),
-                                                            namefiled,
-                                                            Container(
-                                                                child: typemap[
-                                                                            'image'] ==
-                                                                        true
-                                                                    ? imageob
-                                                                    : txtob),
-                                                            Container(
-                                                                child: _controller
-                                                                        .value
-                                                                        .text
-                                                                        .isNotEmpty
-                                                                    ? convert
-                                                                    : Text(
-                                                                        'please enter a name',
-                                                                        style:
-                                                                            defaultStyle,
-                                                                      ))
-                                                          ],
-                                                        )
-                                                      : //
-                                                      mesgimg,
-                                                ),
-                                              ],
-                                            )
-                                          : //
-                                          SizedBox(
-                                              height: MediaQuery.of(context)
-                                                      .size
-                                                      .height -
-                                                  90,
-                                              child: Center(
-                                                  child: Column(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25)),
+                            color: const Color(0xff22273E).withOpacity(0.7)),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: imagebytes == null
+                              ? SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height - 90,
+                                  child: done == false ? home : mesgtxt)
+                              : Column(
+                                  children: [
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                    ),
+                                    Container(
+                                        child: prograss == false
+                                            ? Column(
                                                 mainAxisAlignment:
                                                     MainAxisAlignment.center,
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.center,
                                                 children: [
+                                                  Image.memory(
+                                                    imagebytes!,
+                                                    width:
+                                                        MediaQuery.of(context)
+                                                            .size
+                                                            .width,
+                                                    alignment: Alignment.center,
+                                                  ),
                                                   Container(
-                                                      margin:
-                                                          const EdgeInsets.all(
-                                                              50.0),
-                                                      child: const Center(
-                                                        child: Center(
-                                                          child: Text(
-                                                              'This may take a few minutes to complete',
-                                                              style: TextStyle(
-                                                                  color: Colors
-                                                                      .grey,
-                                                                  fontSize:
-                                                                      18.0)),
-                                                        ),
-                                                      )),
-                                                  SizedBox(
-                                                      height: 50,
-                                                      child: Column(
-                                                        children: const [
-                                                          CircularProgressIndicator(),
-                                                        ],
-                                                      )),
+                                                    child: done == false
+                                                        ? Column(
+                                                            children: [
+                                                              change,
+                                                              Column(
+                                                                children: [
+                                                                  // ignore: prefer_const_constructors
+                                                                  Text(
+                                                                    "output type",
+                                                                    style: const TextStyle(
+                                                                        color: Colors
+                                                                            .white60,
+                                                                        fontSize:
+                                                                            16,
+                                                                        fontWeight:
+                                                                            FontWeight.bold),
+                                                                  ),
+                                                                  Wrap(
+                                                                    spacing: 3,
+                                                                    runSpacing:
+                                                                        3,
+                                                                    children:
+                                                                        typeList,
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                              namefiled,
+                                                              Container(
+                                                                  child: typemap[
+                                                                              'image'] ==
+                                                                          true
+                                                                      ? imageob
+                                                                      : txtob),
+                                                              Container(
+                                                                  child: _controller
+                                                                          .value
+                                                                          .text
+                                                                          .isNotEmpty
+                                                                      ? convert
+                                                                      : Text(
+                                                                          'please enter a name',
+                                                                          style:
+                                                                              defaultStyle,
+                                                                        ))
+                                                            ],
+                                                          )
+                                                        : //
+                                                        mesgimg,
+                                                  ),
                                                 ],
+                                              )
+                                            : //
+                                            SizedBox(
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height -
+                                                    90,
+                                                child: Center(
+                                                    child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.center,
+                                                  children: [
+                                                    Container(
+                                                        margin: const EdgeInsets
+                                                            .all(50.0),
+                                                        child: const Center(
+                                                          child: Center(
+                                                            child: Text(
+                                                                'This may take a few minutes to complete',
+                                                                style: TextStyle(
+                                                                    color: Colors
+                                                                        .grey,
+                                                                    fontSize:
+                                                                        18.0)),
+                                                          ),
+                                                        )),
+                                                    SizedBox(
+                                                        height: 50,
+                                                        child: Column(
+                                                          children: const [
+                                                            CircularProgressIndicator(),
+                                                          ],
+                                                        )),
+                                                  ],
+                                                )),
                                               )),
-                                            )),
-                                ],
-                              ),
+                                  ],
+                                ),
+                        ),
                       ),
                     ),
-                  ),
-                )),
-              ),
+                  )),
             ],
           ),
         ),
